ada5052da97791327575260b0272be09b36760bd
JordanLandry <jordanlandry2001@gmail.com>
2024-10-11 14:42:20 -0400

    Updates navbar styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import "./styles/App.scss";
+import { ErrorBoundary } from "./components/ErrorBoundary";
+import Navbar from "./components/Navbar";
 import AboutMe from "./sections/AboutMe";
 import ProjectSection from "./sections/ProjectSection";
 import WorkExperience from "./sections/WorkExperience";
-import ChessPage from "./pages/chessPage/ChessPage";
-import { ErrorBoundary } from "./components/ErrorBoundary";
-import GuitarHeroPage from "./pages/guitarHeroPage/GuitarHeroPage";
-import RubiksCubePage from "./pages/rubiksCubePage/RubiksCubePage";
-import Navbar from "./components/Navbar";
+import "./styles/App.scss";
 
 function App() {
   return (
     <>
       <ErrorBoundary>
-        <Navbar />
         <AboutMe />
+        <Navbar />
         <WorkExperience />
         <ProjectSection />
-
-        {/* <ChessPage /> */}
-        {/* <GuitarHeroPage /> */}
-        {/* <RubiksCubePage /> */}
       </ErrorBoundary>
     </>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,8 @@
 import Link from "./Link";
+import "./navbar.scss";
 
 const links = [
-  { href: "#home", text: "Home" },
+  { href: "#home", text: "Home", icon: "favicon.ico" },
   { href: "#about", text: "About" },
   { href: "#work", text: "Work" },
   { href: "#projects", text: "Projects" },
@@ -12,9 +13,10 @@ export default function Navbar() {
   return (
     <nav className="navbar-wrapper">
       <ul className="navbar-list">
-        {links.map(({ href, text }) => (
+        {links.map(({ href, text, icon }) => (
           <li key={href} className="navbar-item">
             <Link href={href} target="_self">
+              {icon ? <img src={icon} alt={text} /> : null}
               {text}
             </Link>
           </li>

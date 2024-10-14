1777cbb2974955cfc21f12eaac2526097d5ab6bc
JordanLandry <jordanlandry2001@gmail.com>
2024-10-14 17:53:46 -0400

    Removes skill section, adds software developer to homeSection

--- src/sections/HomeSection.tsx
+++ src/sections/HomeSection.tsx
@@ -1,12 +1,20 @@
-import Signature from '../components/Signature'
-import './homeSection.scss'
+import FadeInOnScroll from "../components/FadeInOnScroll";
+import Signature from "../components/Signature";
+import "./homeSection.scss";
 
 export default function HomeSection() {
+  const nameAnimationDurationMs = 1500;
+
   return (
     <div className="home-section" id="home">
-      <Signature />
-      <p>
-      </p>
+      <Signature durationMs={nameAnimationDurationMs} />
+
+      <FadeInOnScroll
+        durationMs={nameAnimationDurationMs}
+        delayMs={nameAnimationDurationMs + 500}
+      >
+        <h1>Software Developer</h1>
+      </FadeInOnScroll>
     </div>
-  )
+  );
 }
